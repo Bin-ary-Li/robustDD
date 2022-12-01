3f093ac9edd0e518f24a2361dfab865ba65be352
Bin Li <bin.li960305@gmail.com>
2022-11-30 22:23:07 -0500

    camelyon train script

---
+++ wilds_dd/run.py
@@ -0,0 +1,39 @@
+import os
+import argparse
+
+def doArgs():
+    parser = argparse.ArgumentParser(description='run 4 trainings at a time')
+
+    parser.add_argument('--path', type=str, help="python file path, default='./resnet50.py'", default='cnn_train.py')
+    parser.add_argument('--dataset', type=str, help="name of the dataset to use, support: cifar10", default='cifar10')
+    parser.add_argument('--cls', type=int, help="num of classes", required=True)
+    parser.add_argument('--hparam', type=str, help="hparameter of model complexity, control densenet growth rate, if pass multiple one use comma to seperate", required=True)
+    parser.add_argument('--savepath', type=str, help="save path for log and model checkpoint, default to ./logs", default='logs')
+    parser.add_argument('--epoch', type=int, help="max number of epoch to run, default 10", default=10)
+    parser.add_argument('--noise', type=float, help="add label noise, 0 means no noise and 1 is all noise", default=0)
+    return parser.parse_args()
+
+def main():
+    args = doArgs()
+
+    path = args.path
+
+    width = [int(item) for item in args.width.split(',')]
+
+    n_cls = args.cls
+
+    dataset = args.dataset
+
+    savepath = args.savepath
+
+    epoch = args.epoch 
+    
+    noise = args.noise
+
+    for i in width:
+        command = f'python {path} --dataset {dataset} --cls {n_cls} --hparam {i} --savepath {savepath} --epoch {epoch} --noise {noise}'
+        os.system(command)
+
+
+if __name__ == '__main__':
+    main()
--- wilds_dd/wilds_camelyon_train.py
+++ wilds_dd/wilds_camelyon_train.py
@@ -1,76 +1,122 @@
-import torchvision
-import torch
+from wilds import get_dataset
+from wilds.common.data_loaders import get_train_loader
+from wilds.common.data_loaders import get_eval_loader
 import torchvision.transforms as transforms
-import torch.nn as nn
 from torchvision.models import DenseNet
+import torch
 import pytorch_lightning as pl
-import numpy as np
-import torchmetrics
 import argparse
-import os
 
 # define the LightningModule
 class LitDensenet(pl.LightningModule):
     ''' Returns a Densenet121 with growth parameter k. '''
-    def __init__(self, k=32, num_classes=10, lr=1e-4):
+    def __init__(self, trainset, testsets, testset_names, 
+                k=32, num_classes=10, lr=1e-4, train_batch_size=256, test_batch_size=256):
         super().__init__()
-        self.densenet = DenseNet(growth_rate=k)
+        self.trainset = trainset
+        self.testsets = testsets
+        self.testset_names = testset_names
+        self.train_batch_size = train_batch_size
+        self.test_batch_size = test_batch_size
+        self.model = DenseNet(growth_rate=k, num_classes=num_classes)
         self.lr = lr
 
+    def train_dataloader(self):
+        train_loader=  get_train_loader("standard", self.trainset, 
+                                batch_size=self.train_batch_size,
+                                num_workers=8, pin_memory=True)
+        return train_loader
+    
+    def val_dataloader(self):
+        val_loaders = []
+        for dataset in self.testsets:
+            loader = get_eval_loader("standard", dataset, 
+                                batch_size=self.test_batch_size,
+                                num_workers=8, pin_memory=True)
+            val_loaders.append(loader)
+        return val_loaders
+
     def training_step(self, batch, batch_idx):
-        # training_step defines the train loop.
-        # it is independent of forward
-        inputs, labels = batch
+        inputs, labels, metadata = batch
         inputs = inputs.cuda()
         labels = labels.cuda()
-        outputs = self.cnn(inputs)
+        outputs = self.model(inputs)
         loss = torch.nn.functional.cross_entropy(outputs, labels)
 
-        _, preds = torch.max(outputs.data, 1)
-        acc = torchmetrics.functional.accuracy(preds, labels)
+        _, y_pred = torch.max(outputs.data, 1)
+        return {'loss':loss,
+                'y_pred':y_pred.cpu(),
+                'labels':labels.cpu(),
+                'metadata':metadata.cpu()}
+    
+    def training_epoch_end(self, train_step_outputs):
+        preds = [x["y_pred"] for x in train_step_outputs]
+        labels = [x["labels"] for x in train_step_outputs]
+        metadata = [x["metadata"] for x in train_step_outputs]
+        eval = self.trainset.eval(torch.cat(preds), 
+                            torch.cat(labels), 
+                            torch.cat(metadata))
+        for key, value in eval[0].items():
+            if 'avg' in key or 'wg' in key:
+                self.log(key+'_'+'train', value, on_epoch=True)
+            else:
+                self.log(key,value, on_epoch=True)
 
-        # Logging to TensorBoard by default
-        self.log("train_loss", loss)
-        self.log("train_acc", acc)
-        return loss
 
-    def validation_step(self, batch, batch_idx):
-        # training_step defines the train loop.
-        # it is independent of forward
-        inputs, labels = batch
+    def validation_step(self, batch, batch_idx, dataloader_idx):
+        inputs, labels, metadata = batch
         inputs = inputs.cuda()
         labels = labels.cuda()
-        outputs = self.cnn(inputs)
+        outputs = self.model(inputs)
         loss = torch.nn.functional.cross_entropy(outputs, labels)
-        
-        _, preds = torch.max(outputs.data, 1)
-        acc = torchmetrics.functional.accuracy(preds, labels)
 
-        # Logging to TensorBoard by default
-        self.log("test_loss", loss)
-        self.log("test_acc", acc)
-        return loss
+        _, y_pred = torch.max(outputs.data, 1)
+        return {'loss':loss,
+                'test_idx':dataloader_idx,
+                'y_pred':y_pred.cpu(),
+                'labels':labels.cpu(),
+                'metadata':metadata.cpu()}
+
+    def validation_epoch_end(self, val_step_outputs) -> None:
+        for k in range(len(self.testsets)):
+            preds = []
+            labels = []
+            metadata = []
+            for x in val_step_outputs[k]:
+                preds.append(x['y_pred'])
+                labels.append(x['labels'])
+                metadata.append(x['metadata'])
+            eval = self.testsets[k].eval(torch.cat(preds), 
+                                        torch.cat(labels), 
+                                        torch.cat(metadata))
+            # Logging
+            testset_name = self.testset_names[k]
+            for key, value in eval[0].items():
+                if 'avg' in key or 'wg' in key:
+                    self.log(key+'_'+testset_name, value)
+                else:
+                    self.log(key,value)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         return optimizer
 
-def generate_noisy_label(labels, p, n_cls):
-    noisy_targets = labels.copy()
-    for i, target in enumerate(labels):
-        if np.random.random_sample() <= p:
-            incorrect_labels = [i for i in range(n_cls) if i != target]
-            np.random.shuffle(incorrect_labels)
-            noisy_targets[i] = incorrect_labels[0]
-    return noisy_targets
+# def generate_noisy_label(labels, p, n_cls):
+#     noisy_targets = labels.copy()
+#     for i, target in enumerate(labels):
+#         if np.random.random_sample() <= p:
+#             incorrect_labels = [i for i in range(n_cls) if i != target]
+#             np.random.shuffle(incorrect_labels)
+#             noisy_targets[i] = incorrect_labels[0]
+#     return noisy_targets
 
 def doArgs():
     parser = argparse.ArgumentParser(description='parameters for Double Decent')
-    parser.add_argument('--dataset', type=str, help="the name of the dataset to use, currently support one of these strings: [cifar10]", required=True)
-    parser.add_argument('--noise', type=float, help='making the training label noisy, 0 means no noise', default=0)
+    parser.add_argument('--dataset', type=str, help="the name of the dataset to use, currently support one of these strings: [camelyon17]", default='camelyon17')
+    parser.add_argument('--noise', type=float, help='making the training label noisy, NO SUPPORTED for now!', default=0)
     parser.add_argument('--savepath', type=str, help="save path for log and model checkpoint, default to ./logs", default='logs')
     parser.add_argument('--cls', type=int, help="num of classes", required=True)
-    parser.add_argument('--width', type=int, help="width of model", required=True)
+    parser.add_argument('--hparam', type=int, help="hparameter of model complexity, control densenet growth rate", required=True)
     parser.add_argument('--epoch', type=int, help="max number of epoch to run, default 10", default=10)
     parser.add_argument('--resumeckpt', type=str, help='path of model checkpoint to resume', default='')
     parser.add_argument('--loadnoisydata', type=str, help='load train data that has noisy label (to guarantee reproducibility when resume training)', default='')
@@ -84,7 +130,7 @@ def main():
     batch_size = 256
     
     max_epoch = args.epoch
-    width = args.width
+    growth_rate = args.hparam
     n_cls = args.cls
     dataset = args.dataset.lower()
     label_noise = args.noise
@@ -92,56 +138,79 @@ def main():
     noisy_data_path = args.loadnoisydata
     save_path = args.savepath
 
-    # mod = resnet50(width_per_group=width)
-    # mod.fc = torch.nn.Linear(2048, n_cls)
-    mod = make_cnn(width, n_cls)
-    litmod = LitCNN(mod, lr=lr)
+    # Load the full dataset, and download it if necessary
+    dataset = get_dataset(dataset=dataset, download=True)
 
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    trans = transforms.Compose(
+            [transforms.ToTensor()]
+        )
 
-    if (dataset == 'cifar10'):
-        trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                                download=True, transform=transform)
-        testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                                download=True, transform=transform)
-    else:
-        return
+    train_data = dataset.get_subset(
+        "train",
+        frac=1,
+        transform=trans,
+    )
 
-    logger = pl.loggers.CSVLogger(save_path, name=f"cnn_width{width}_noise{label_noise}")
+    # Get the test set
+    test_data = dataset.get_subset(
+        "test",
+        transform=trans
+    )
 
-    # check if want to resume training
-    if (resume_ckpt != ''):
-        # if load noisy data for resuming training
-        if label_noise > 0:
-            assert noisy_data_path != ''
-            trainset = torch.load(noisy_data_path)
-    else:
-        # If generating noisy data for the first time, save it to the logging directory
-        if (noisy_data_path == '' and label_noise > 0):
-            noisy_labels = generate_noisy_label(trainset.targets, label_noise, n_cls)
-            trainset.targets = noisy_labels
-            os.makedirs(logger.root_dir)
-            torch.save(trainset, os.path.join(logger.root_dir, 'noisy_train.pt'))
+    val_data = dataset.get_subset(
+        'val',
+        transform=trans
+    )
+
+    id_val_data = dataset.get_subset(
+        'id_val',
+        transform=trans
+    )
+
+    test_datasets = [test_data, val_data, id_val_data]
+    test_split_names = ['test', 'val', 'idval']
+
+    model = LitDensenet(train_data, test_datasets, test_split_names, 
+                        k=growth_rate, num_classes=n_cls, lr=lr, train_batch_size=batch_size,
+                        test_batch_size=batch_size)
+
+    logger = pl.loggers.CSVLogger('logs', 
+                            name=f"densenet_width{growth_rate}_noise{label_noise}")
+
+    # # check if want to resume training
+    # if (resume_ckpt != ''):
+    #     # if load noisy data for resuming training
+    #     if label_noise > 0:
+    #         assert noisy_data_path != ''
+    #         trainset = torch.load(noisy_data_path)
+    # else:
+    #     # If generating noisy data for the first time, save it to the logging directory
+    #     if (noisy_data_path == '' and label_noise > 0):
+    #         noisy_labels = generate_noisy_label(trainset.targets, label_noise, n_cls)
+    #         trainset.targets = noisy_labels
+    #         os.makedirs(logger.root_dir)
+    #         torch.save(trainset, os.path.join(logger.root_dir, 'noisy_train.pt'))
 
 
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                        shuffle=True, num_workers=2)
+    # trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+    #                                     shuffle=True, num_workers=2)
 
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                        shuffle=False, num_workers=2)
+    # testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+    #                                     shuffle=False, num_workers=2)
 
     trainer = pl.Trainer(
         max_epochs=max_epoch,
-        accelerator="auto",
+        accelerator="gpu",
+        precision=16,
         logger=logger,
-        devices=1)
+        devices=1,)
     
-    if (resume_ckpt == ''):
-        trainer.fit(litmod, trainloader, testloader)
-    else:
-        trainer.fit(litmod, trainloader, testloader, ckpt_path=resume_ckpt)
+    # if (resume_ckpt == ''):
+    #     trainer.fit(litmod, trainloader, testloader)
+    # else:
+    #     trainer.fit(litmod, trainloader, testloader, ckpt_path=resume_ckpt)
+
+    trainer.fit(model)
 
 
 if __name__ == '__main__':
