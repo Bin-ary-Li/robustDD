bc240e098d246232cd95ee080453974df08dd6d5
Bin Li <bin.li960305@gmail.com>
2022-12-01 01:21:47 -0500

    running code

--- wilds_dd/run.py
+++ wilds_dd/run.py
@@ -4,13 +4,14 @@ import argparse
 def doArgs():
     parser = argparse.ArgumentParser(description='run 4 trainings at a time')
 
-    parser.add_argument('--path', type=str, help="python file path, default='./resnet50.py'", default='cnn_train.py')
-    parser.add_argument('--dataset', type=str, help="name of the dataset to use, support: cifar10", default='cifar10')
-    parser.add_argument('--cls', type=int, help="num of classes", required=True)
+    parser.add_argument('--path', type=str, help="python file path", default='wilds_camelyon_train.py')
+    parser.add_argument('--dataset', type=str, help="name of the dataset to use, support: camelyon17", default='camelyon17')
+    parser.add_argument('--cls', type=int, help="num of classes", default=2)
     parser.add_argument('--hparam', type=str, help="hparameter of model complexity, control densenet growth rate, if pass multiple one use comma to seperate", required=True)
     parser.add_argument('--savepath', type=str, help="save path for log and model checkpoint, default to ./logs", default='logs')
     parser.add_argument('--epoch', type=int, help="max number of epoch to run, default 10", default=10)
     parser.add_argument('--noise', type=float, help="add label noise, 0 means no noise and 1 is all noise", default=0)
+    parser.add_argument('--worker', type=int, help="number of worker", default=2)
     return parser.parse_args()
 
 def main():
@@ -18,7 +19,7 @@ def main():
 
     path = args.path
 
-    width = [int(item) for item in args.width.split(',')]
+    hparam = [int(item) for item in args.hparam.split(',')]
 
     n_cls = args.cls
 
@@ -30,8 +31,10 @@ def main():
     
     noise = args.noise
 
-    for i in width:
-        command = f'python {path} --dataset {dataset} --cls {n_cls} --hparam {i} --savepath {savepath} --epoch {epoch} --noise {noise}'
+    worker = args.worker
+
+    for i in hparam:
+        command = f'python {path} --dataset {dataset} --cls {n_cls} --hparam {i} --savepath {savepath} --epoch {epoch} --noise {noise} --worker {worker}'
         os.system(command)
 
 
