c7608eaf6ca3c60481e7ceed45dc2a5d3fc4cc68
Bin Li <bin.li960305@gmail.com>
2022-12-13 00:10:33 -0500

    random augment

--- wilds_dd/run.py
+++ wilds_dd/run.py
@@ -7,6 +7,7 @@ def doArgs():
     parser.add_argument('--path', type=str, help="python file path", default='wilds_camelyon_train.py')
     parser.add_argument('--dataset', type=str, help="name of the dataset to use, support: camelyon17", default='camelyon17')
     parser.add_argument('--cls', type=int, help="num of classes", default=2)
+    parser.add_argument('--randaugment', type=int, help="add random augmentation to training data, 1 means add augmentation", default=0)
     parser.add_argument('--hparam', type=str, help="hparameter of model complexity, control densenet growth rate, if pass multiple one use comma to seperate", required=True)
     parser.add_argument('--savepath', type=str, help="save path for log and model checkpoint, default to ./logs", default='logs')
     parser.add_argument('--epoch', type=int, help="max number of epoch to run, default 10", default=10)
@@ -34,7 +35,7 @@ def main():
     worker = args.worker
 
     for i in hparam:
-        command = f'python {path} --dataset {dataset} --cls {n_cls} --hparam {i} --savepath {savepath} --epoch {epoch} --noise {noise} --worker {worker}'
+        command = f'python {path} --dataset {dataset} --cls {n_cls} --hparam {i} --savepath {savepath} --epoch {epoch} --noise {noise} --worker {worker} --randaugment {args.randaugment}'
         os.system(command)
 
 
